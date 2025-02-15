c43c4c3270db7cd64328c12830a06e707a15662c
Bill Nagbe <BillyNagbe@gmail.com>
2025-02-15 08:16:16 -0600

    nextjs starter project

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,16 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+// module.exports = {
+//   images: {
+//     remotePatterns: [ {
+//       protocol: 'https',
+//       hostname: '://ricoinc.com/cdn/shop/products/SSP94001I_800x.jpg?v=1660005859',
+//       port: '',
+//       pathname: '',
+//       search: ''
+//     }]
+//   }
+// }
+
 export default nextConfig;
